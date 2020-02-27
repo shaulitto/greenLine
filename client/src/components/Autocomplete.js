fab74b9d9d5d5a0f3a8318237828a0e02806c4b0
borndead <shaulitto1@gmail.com>
2020-02-27 15:55:17 +0100

    Autocomplete

---
+++ client/src/components/Autocomplete.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Autocomplete = props => {
+  const options = props.results.map(r => (
+    <li
+      key={r.id}
+      onClick={e => {
+        props.updateText(e.target.innerText);
+      }}
+    >
+      {r.name}
+    </li>
+  ));
+  return (
+    <>
+      <input
+        name={props.name}
+        id={props.id}
+        onChange={props.handleInputChange}
+        value={props.value}
+      />
+      <ul>{options}</ul>
+    </>
+  );
+};
+
+export default Autocomplete;
