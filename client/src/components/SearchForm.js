47d8f81aaab44373b03d8cb9984c9054717de9b7
borndead <shaulitto1@gmail.com>
2020-03-01 11:22:12 +0100

    merge dev attempt

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Autocomplete from "./Autocomplete";
-import { debounce } from "lodash";
 
 export class SearchForm extends Component {
   state = {
