c4ac11cbd15eb597a9cb4d3a709104bba4da1b41
borndead <shaulitto1@gmail.com>
2020-03-03 09:50:43 +0100

    fixed error in seeds.js

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
 console.log("Connected to DB");
-const stations = require("./full.json.js");
+const stations = require("./full.json");
 
 const Station = require("../models/Station");
 stations.forEach(elem => {
