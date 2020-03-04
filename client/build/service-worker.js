31c6b8ec64be13ceada1384d2e764e8921a04310
borndead <shaulitto1@gmail.com>
2020-03-04 17:10:25 +0100

    update master fe attempt again

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e59121d49ef93160e51d819a7da9dce0.js"
+  "/precache-manifest.02b34b8b47e91966dd0ea48051964983.js"
 );
 
 self.addEventListener('message', (event) => {
