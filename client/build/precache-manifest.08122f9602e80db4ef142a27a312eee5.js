d4c61d88427f7b06bc1cf3918afc9e4d2dd21416
borndead <shaulitto1@gmail.com>
2020-03-05 19:52:22 +0100

    deploy attempt

---
+++ client/build/precache-manifest.08122f9602e80db4ef142a27a312eee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3c542033754d00e167ae0f28b6444a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60fa7e5875b03902bd5d",
+    "url": "/static/css/main.e59dbf8e.chunk.css"
+  },
+  {
+    "revision": "39a09415606faaf65fd1",
+    "url": "/static/js/2.b2018dce.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b2018dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fa7e5875b03902bd5d",
+    "url": "/static/js/main.20261638.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
