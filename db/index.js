b29074d78a41a5f612060c27a1aa1c2588a04b17
shaulitto <58592695+shaulitto@users.noreply.github.com>
2020-11-23 17:07:29 +0100

    Update index.js

--- db/index.js
+++ db/index.js
@@ -6,8 +6,7 @@ mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
 
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(conn => {
+  .connect(process.env.DB_URI || 'mongodb://localhost:27017/greenLine'
     console.log(`Connected to ${conn.connections[0].name}`);
   })
   .catch(err => {
